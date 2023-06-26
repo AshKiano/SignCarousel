a2243a9131a72b8dc75c2adfc4bbe11fdf466f1f
AshKiano <ash.kiano@gmail.com>
2023-06-26 12:00:56 +0200

    version 1.4

--- src/main/java/com/ashkiano/signcarousel/SignCarousel.java
+++ src/main/java/com/ashkiano/signcarousel/SignCarousel.java
@@ -3,6 +3,7 @@ package com.ashkiano.signcarousel;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Sign;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -11,35 +12,45 @@ import org.bukkit.ChatColor;
 import java.util.List;
 import java.util.Map;
 
-// Main plugin class that extends JavaPlugin.
+// Main class of the SignCarousel plugin
 public class SignCarousel extends JavaPlugin {
 
-    // This method is called when the plugin is enabled.
+    // Called when the plugin is enabled
     @Override
     public void onEnable() {
+        ConfigurationSection messagesConfig = null;
+
+        // Set language to English
+        String language = "en";
+
         try {
-            // Save the default configuration if it doesn't exist yet.
+            // Save default configuration if it doesn't exist
             saveDefaultConfig();
 
+            // Initialize Metrics for plugin analytics
             Metrics metrics = new Metrics(this, 18812);
 
-            // Load the configuration.
+            // Load the main configuration
             FileConfiguration config = getConfig();
-            // Get list of sign configurations from the config file.
+
+            // Retrieve the list of sign configurations from the main configuration
             List<Map<?, ?>> signConfigs = config.getMapList("signs");
 
-            // Loop over each sign configuration.
+            // Load the messages configuration
+            messagesConfig = getConfig().getConfigurationSection("messages");
+
+            // Loop over each sign configuration
             for (Map<?, ?> signConfig : signConfigs) {
                 // Process each sign configuration inside a try-catch block.
                 try {
-                    // Get the list of messages for this sign.
+                    // Retrieve the messages, delay, and typewriting effect settings from the sign configuration
                     List<List<String>> messages = (List<List<String>>) signConfig.get("messages");
                     // Get the delay between messages for this sign.
                     int delay = (int) signConfig.get("delay");
                     // Check if typewriting effect is enabled for this sign.
                     boolean useTypewritingEffect = (boolean) signConfig.get("typewriting_effect");
 
-                    // Get the location of this sign.
+                    // Retrieve the location of the sign from the sign configuration
                     Map<?, ?> locationData = (Map<?, ?>) signConfig.get("location");
                     String worldName = (String) locationData.get("world");
                     World world = getServer().getWorld(worldName);
@@ -49,31 +60,31 @@ public class SignCarousel extends JavaPlugin {
                     int z = (int) locationData.get("z");
 
                     Location signLocation = new Location(world, x, y, z);
-                    // Check if the block at the given location is a sign.
+
+                    // If the block at the location is not a sign, log a warning and continue with the next configuration
                     if (!(signLocation.getBlock().getState() instanceof Sign)) {
-                        // If not, log a warning and skip this sign.
-                        getLogger().warning("Block at " + signLocation + " is not a sign!");
+                        getLogger().warning(String.format(messagesConfig.getString("not_a_sign." + language), signLocation));
                         continue;
                     }
-                    // Get the sign at the given location.
+
+                    // Get the sign at the location
                     Sign sign = (Sign) signLocation.getBlock().getState();
 
-                    // Create a new SignCarouselTask for this sign and schedule it to run periodically.
+                    // Create a new SignCarouselTask for the sign and schedule it to run periodically
                     new SignCarouselTask(sign, messages, delay, useTypewritingEffect).runTaskTimer(this, 0L, delay * 20L);
                 } catch (Exception e) {
-                    // Log any exceptions that occur during sign configuration processing.
-                    getLogger().warning("Error processing sign configuration: " + e.getMessage());
+                    // If there is an error processing the sign configuration, log a warning
+                    getLogger().warning(String.format(messagesConfig.getString("config_error." + language), e.getMessage()));
                 }
             }
         } catch (Exception e) {
-            // Log any exceptions that occur during plugin initialization.
-            getLogger().severe("Error initializing plugin: " + e.getMessage());
-            // Disable the plugin to prevent further issues.
+            // If there is an error initializing the plugin, log a severe error and disable the plugin
+            getLogger().severe(String.format(messagesConfig.getString("init_error." + language), e.getMessage()));
             getServer().getPluginManager().disablePlugin(this);
         }
     }
 
-    // This class represents a task that updates a sign's text periodically.
+    // Task that updates a sign's text periodically
     private class SignCarouselTask extends BukkitRunnable {
 
         // The sign that this task updates.
@@ -85,31 +96,32 @@ public class SignCarousel extends JavaPlugin {
         // The index of the current message in the list of messages.
         private int currentIndex = 0;
 
-        // Create a new SignCarouselTask.
+        // Create a new SignCarouselTask with the given sign, messages, delay, and typewriting effect setting
         public SignCarouselTask(Sign sign, List<List<String>> messages, int delay, boolean useTypewritingEffect) {
             this.sign = sign;
             this.messages = messages;
             this.useTypewritingEffect = useTypewritingEffect;
         }
 
-        // This method is called when the task is run.
+        // Called periodically to update the sign's text
         @Override
         public void run() {
-            // If we've reached the end of the list of messages, start over.
+            // Reset the current index if it's past the end of the messages list
             if (currentIndex >= messages.size()) {
                 currentIndex = 0;
             }
 
-            // Get the current message.
+            // Get the current message
             List<String> currentMessage = messages.get(currentIndex);
 
-            // Clear the old message from the sign.
+            // Clear the sign's text
             for (int i = 0; i < 4; i++) {
                 sign.setLine(i, "");
             }
             sign.update();
 
-            // Write the new message to the sign.
+            // If the typewriting effect is enabled, start a new TypewriterEffect task
+            // Otherwise, set the sign's text to the current message
             if (useTypewritingEffect) {
                 // If the typewriting effect is enabled, use a TypewriterEffect task to display the message.
                 new TypewriterEffect(sign, currentMessage).runTaskTimer(SignCarousel.this, 0L, 2L);
@@ -121,12 +133,12 @@ public class SignCarousel extends JavaPlugin {
                 sign.update();
             }
 
-            // Move on to the next message.
+            // Move on to the next message
             currentIndex++;
         }
     }
 
-    // This class represents a task that displays a message on a sign using a typewriter effect.
+    // Task that applies the typewriting effect to a sign's text
     private class TypewriterEffect extends BukkitRunnable {
 
         // The sign that this task updates.
@@ -138,25 +150,26 @@ public class SignCarousel extends JavaPlugin {
         // The index of the current character in the current line.
         private int currentCharIndex = 0;
 
-        // Create a new TypewriterEffect.
+        // Create a new TypewriterEffect with the given sign and lines
         public TypewriterEffect(Sign sign, List<String> lines) {
             this.sign = sign;
             this.lines = lines;
         }
 
-        // This method is called when the task is run.
+        // Called periodically to add one character at a time to the sign's text
         @Override
         public void run() {
-            // If we've reached the end of the list of lines, cancel the task.
+            // If all lines have been processed, cancel the task
             if (currentLineIndex >= lines.size()) {
                 cancel();
                 return;
             }
 
-            // Get the current line.
+            // Get the current line
             String currentLine = lines.get(currentLineIndex);
 
-            // If we've reached the end of the current line, move on to the next line.
+            // If all characters in the current line have been processed, move on to the next line
+            // Otherwise, add the next character to the sign's text
             if (currentCharIndex > currentLine.length()) {
                 currentCharIndex = 0;
                 currentLineIndex++;
