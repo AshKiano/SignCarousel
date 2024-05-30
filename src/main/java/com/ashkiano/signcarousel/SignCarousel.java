2b6f4ab9bafe6c5f8780d16588ab34d051e918eb
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-05-30 21:18:03 +0200

    version 1.14

--- src/main/java/com/ashkiano/signcarousel/SignCarousel.java
+++ src/main/java/com/ashkiano/signcarousel/SignCarousel.java
@@ -197,42 +197,44 @@ public class SignCarousel extends JavaPlugin {
     }
 
     private void checkForUpdates() {
-        try {
-            String pluginName = this.getDescription().getName();
-            URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
+        new Thread(() -> {
+            try {
+                String pluginName = this.getDescription().getName();
+                URL url = new URL("https://www.ashkiano.com/version_check.php?plugin=" + pluginName);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
 
-            int responseCode = con.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
+                int responseCode = con.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
 
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-                String jsonResponse = response.toString();
-                JSONObject jsonObject = new JSONObject(jsonResponse);
-                if (jsonObject.has("error")) {
-                    this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
-                } else {
-                    String latestVersion = jsonObject.getString("latest_version");
-
-                    String currentVersion = this.getDescription().getVersion();
-                    if (currentVersion.equals(latestVersion)) {
-                        this.getLogger().info("This plugin is up to date!");
-                    } else {
-                        this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
                     }
+                    in.close();
+
+                    String jsonResponse = response.toString();
+                    JSONObject jsonObject = new JSONObject(jsonResponse);
+                    if (jsonObject.has("error")) {
+                        this.getLogger().warning("Error when checking for updates: " + jsonObject.getString("error"));
+                    } else {
+                        String latestVersion = jsonObject.getString("latest_version");
+
+                        String currentVersion = this.getDescription().getVersion();
+                        if (currentVersion.equals(latestVersion)) {
+                            this.getLogger().info("This plugin is up to date!");
+                        } else {
+                            this.getLogger().warning("There is a newer version (" + latestVersion + ") available! Please update!");
+                        }
+                    }
+                } else {
+                    this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
                 }
-            } else {
-                this.getLogger().warning("Failed to check for updates. Response code: " + responseCode);
+            } catch (Exception e) {
+                this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
             }
-        } catch (Exception e) {
-            this.getLogger().warning("Failed to check for updates. Error: " + e.getMessage());
-        }
+        }).start();
     }
 }
